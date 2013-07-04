09277f0ded244f775ba99558fa08308e13ea6038
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-04 03:05:27 +0000

    Configure RSpec and Cucumber
    * Generate files for RSpec and Cucumber
    * Set up Database Cleaner
    * Add Factory for User object
    * Add Devise test helpers
    * Enable Email Spec
    * Don't generate RSpec tests for views and helpers
    * Generate step definitions for testing e-mail
    * Run single Cucumber features easily
    * Add Cucumber features

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name 'Test User'
+    email 'example@example.com'
+    password 'changeme'
+    password_confirmation 'changeme'
+    confirmed_at Time.now
+  end
+end
