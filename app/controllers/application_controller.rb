ce3c46802914d39fb36d742d5b0da13932c7cee9
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-04 07:16:41 +0000

    Add name attribute to User model
    * Configure permitted_parameters for Devise
      - Add User.name attribute to permitted parameters for the sign_up and
        account_update actions in the devise controller.
    
    * Create custom views for user registration
      - Add name field to sign_up and account_update registration forms.
      - Add Bootstrap styles to sign_up and account_update registration
        forms.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+  end
 end
