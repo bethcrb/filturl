dd9a51de30490f6b28a0eca96992c34d42d078dd
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-04 06:54:22 +0000

    Set up authorization with CanCan and Rolify
    * CanCan
      - Create CanCan Ability class
      - Grant permission to admins to manage any page
      - Configure CanCan exception handling
    
    * Rolify
      - Create Role model
      - Add methods to User model
      - Generate migration for roles table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  rolify
   devise :confirmable, :database_authenticatable, :lockable,
          :omniauthable, :recoverable, :registerable, :rememberable,
          :timeoutable, :token_authenticatable, :trackable, :validatable
