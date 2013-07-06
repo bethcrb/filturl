1d2d7c50c6f042510b9e68f3bcf6869556140a0c
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-06 15:51:39 -0700

    Clean up URL form JS
    Clean up URL form JS and use URI.js library to normalize urls

--- app/assets/javascripts/globals.js
+++ app/assets/javascripts/globals.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
-  $("input#webpage_url").blur(function() {
-    var input = $(this);
-    var val = input.val();
-    if (val && !val.match(/^http([s]?):\/\/.*/)) {
-        input.val('http://' + val);
+  $("input#webpage_request_url").blur(function() {
+    var request_url = $(this).val();
+
+    var uri = new URI(request_url);
+    if (uri.protocol() == "") {
+      // Default to http if a protocol is not present
+      uri.protocol("http");
+
+      // Create a new URI object so that all normalizations get applied in one go
+      request_url = uri.normalize().toString();
+      uri = new URI(request_url);
     }
+
+    request_url = uri.normalize().toString();
+    $(this).val(request_url);
   });
 });
