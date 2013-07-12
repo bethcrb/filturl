53fdb2719011fea529f0dbb77d4151ef002307e0
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-12 09:43:48 -0700

    Add show/create logic to WebpageRequests
    - Add logic and specs for show and create to WebpageRequests controller
    - Add preliminary show view

--- app/controllers/webpage_requests_controller.rb
+++ app/controllers/webpage_requests_controller.rb
@@ -1,7 +1,26 @@
 class WebpageRequestsController < ApplicationController
   def show
+    @webpage_request = WebpageRequest.find(params[:id])
   end
 
   def create
+    request_url = webpage_request_params[:url]
+
+    @webpage_request = WebpageRequest.find_or_initialize_by({ :url => request_url, :requestor_id => current_user.id })
+
+    respond_to do |format|
+      if @webpage_request.save
+        format.html { redirect_to @webpage_request }
+        format.json { render action: 'show', status: :created, location: @webpage_request }
+      else
+        format.html { render "home/index", notice: @webpage_request.errors  }
+        format.json { render json: @webpage_request.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+    def webpage_request_params
+      params.require(:webpage_request).permit(:url)
+    end
 end
--- spec/controllers/webpage_requests_controller_spec.rb
+++ spec/controllers/webpage_requests_controller_spec.rb
@@ -6,21 +6,28 @@ describe WebpageRequestsController do
     sign_in @user
 
     @webpage_request = FactoryGirl.create(:webpage_request)
+    @attr = {
+      :url          => "http://www.test.com",
+      :requestor_id => @user.id,
+    }
   end
 
   describe "GET 'show'" do
     it "shows the webpage request" do
-      pending
+      get :show, :id => @webpage_request.id
+      assigns(:webpage_request).should == @webpage_request
     end
   end
 
   describe "POST 'create'" do
     it "creates a new webpage request" do
-      pending
+      post :create, {:webpage_request => @attr}
+      assigns(:webpage_request).should be_a(WebpageRequest)
     end
 
     it "redirects to the webpage request" do
-      pending
+      post :create, {:webpage_request => @attr}
+      response.should redirect_to(WebpageRequest.last)
     end
   end
 end
