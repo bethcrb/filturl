65a8b6f9206c1dff7853663729c2ff168e72c968
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-12 09:21:04 -0700

    Configure permitted params before authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :authenticate_user!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
