0e00e0bb16acbc9937a4b55b741e2be2ea8cc74c
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-20 04:18:47 -0700

    Enable Rememberable for OmniAuth users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
+      remember_me(@user)
+
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
@@ -13,7 +17,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
+
     if @user.persisted?
+      remember_me(@user)
+
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "GitHub") if is_navigational_format?
     else
@@ -22,15 +29,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-	def google_oauth2
-	    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+  def google_oauth2
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 
-	    if @user.persisted?
-	      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-	      sign_in_and_redirect @user, :event => :authentication
-	    else
-	      session["devise.google_data"] = request.env["omniauth.auth"]
-	      redirect_to new_user_registration_url
-	    end
-	end
+    if @user.persisted?
+      remember_me(@user)
+
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 end
