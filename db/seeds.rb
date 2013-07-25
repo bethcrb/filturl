c445ca089db3ca1d0287f4901b9ac841d2fb315a
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-07-25 15:43:48 -0700

    Add test user to DB seeds file
    - Add test_user to db/seeds.rb
    - Update application.yml.example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,26 @@
+# Create roles
 YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by(name: role)
 end
 
-user = User.find_or_create_by(username: "admin", email: ENV['ADMIN_EMAIL'].dup)
-user.name = ENV['ADMIN_NAME'].dup
-user.password = ENV['ADMIN_PASSWORD'].dup
+# Create admin user
+admin_user = User.find_or_create_by(
+  username: "admin",
+  email:    ENV['ADMIN_EMAIL'].dup
+)
+admin_user.name = ENV['ADMIN_NAME'].dup
+admin_user.password = ENV['ADMIN_PASSWORD'].dup
+admin_user.confirmed_at = Time.now
+admin_user.save
+admin_user.add_role :admin
+
+# Create regular user
+user = User.find_or_create_by(
+  username: "test_user",
+  email:    ENV['USER_EMAIL'].dup
+)
+user.name = ENV['USER_NAME'].dup
+user.password = ENV['USER_PASSWORD'].dup
 user.confirmed_at = Time.now
 user.save
-user.add_role :admin
+user.add_role :user
