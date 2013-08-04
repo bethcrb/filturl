85880504d4c92b50e99214f8c2b4b1d025aa48fb
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-08-03 20:48:05 -0700

    DRY up OmniAuth
    Consolidate separate omniauth methods

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,44 +1,28 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Devise::Controllers::Rememberable
 
-  def facebook
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      remember_me(@user)
-
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+  Devise.omniauth_providers.each do |provider|
+    define_method provider.to_s do
+      authenticate_omniauth
     end
   end
 
-  def github
-    @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
+  def authenticate_omniauth
+    omniauth_data = request.env['omniauth.auth']
+    provider = omniauth_data['provider']
+    provider_name = provider == 'google_oauth2' ? 'google' : provider
+
+    @user = User.find_for_omniauth(omniauth_data, current_user)
 
     if @user.persisted?
       remember_me(@user)
 
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "GitHub") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+      if is_navigational_format?
+        set_flash_message(:notice, :success, kind: provider_name.capitalize)
+      end
     else
-      session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
-
-  def google_oauth2
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
-      remember_me(@user)
-
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session["devise.#{provider_name}_data"] = omniauth_data
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,24 +49,29 @@ class User < ActiveRecord::Base
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
     else
       where(conditions).first
     end
   end
 
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  def self.find_for_omniauth(auth, signed_in_resource = nil)
     username = auth.extra.raw_info.username
     if username.blank?
-      username = auth.info.email
+      if auth.provider == 'github'
+        username = auth.extra.raw_info.login
+      else
+        username = auth.info.email
+      end
     end
+
     user = User.find_by(email: auth.info.email)
     unless user
       user = User.new(
         name:     auth.info.name,
         email:    auth.info.email,
         username: username,
-        password: Devise.friendly_token[0,20],
+        password: Devise.friendly_token[0, 20],
       )
       user.skip_confirmation!
       user.save!
@@ -79,70 +84,11 @@ class User < ActiveRecord::Base
 
     authentication.name = auth.info.name
     authentication.email = auth.info.email
-    authentication.nickname = auth.info.nickname
+    authentication.nickname = auth.info.nickname if auth.info.nickname
     authentication.first_name = auth.info.first_name
     authentication.last_name = auth.info.last_name
     authentication.image = auth.info.image
     authentication.raw_info = auth.to_json
-    authentication.save!
-
-    user
-  end
-
-  def self.find_for_github_oauth(auth, signed_in_resource=nil)
-    user = User.find_by(username: auth.extra.raw_info.login)
-    unless user
-      user = User.new(
-        name:     auth.info.name,
-        email:    auth.info.email,
-        username: auth.extra.raw_info.login,
-        password: Devise.friendly_token[0,20],
-      )
-      user.skip_confirmation!
-      user.save!
-    end
-
-    authentication = user.authentications.find_or_create_by(
-      provider: auth.provider,
-      uid:      auth.uid,
-    )
-
-    authentication.name = auth.info.name
-    authentication.email = auth.info.email
-    authentication.nickname = auth.info.nickname
-    authentication.location = auth.info.location
-    authentication.description = auth.info.description
-    authentication.image = auth.info.image
-    authentication.raw_info = auth.extra.raw_info.to_json
-    authentication.save!
-
-    user
-  end
-
-  def self.find_for_google_oauth2(auth, signed_in_resource=nil)
-    user = User.find_by(email: auth.info.email)
-    unless user
-      user = User.new(
-        name:     auth.info.name,
-        email:    auth.info.email,
-        username: auth.info.email,
-        password: Devise.friendly_token[0,20],
-      )
-      user.skip_confirmation!
-      user.save!
-    end
-
-    authentication = user.authentications.find_or_create_by(
-      provider: auth.provider,
-      uid:      auth.uid,
-    )
-
-    authentication.name = auth.info.name
-    authentication.email = auth.info.email
-    authentication.first_name = auth.info.first_name
-    authentication.last_name = auth.info.last_name
-    authentication.image = auth.info.image
-    authentication.raw_info = auth.extra.raw_info.to_json
     authentication.save!
 
     user
