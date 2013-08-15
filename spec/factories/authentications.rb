8e6f41620d5d287f7728706cdf5dd60263c3ffee
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-08-14 19:08:31 -0700

    Add factory for Authentication model

--- spec/factories/authentications.rb
+++ spec/factories/authentications.rb
@@ -1,15 +1,16 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  auth_uid = Random.rand(10**21).to_s
   factory :authentication do
-    provider 'MyString'
-    uid 'MyString'
-    name 'MyString'
-    email 'MyString'
-    nickname 'MyString'
-    first_name 'MyString'
-    last_name 'MyString'
-    image 'MyString'
+    provider 'facebook'
+    uid auth_uid
+    name { "#{first_name} #{last_name}" }
+    email { Faker::Internet.email(name) }
+    nickname { Faker::Internet.user_name(name) }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    image "http://graph.facebook.com/#{auth_uid}/picture?type=square"
     raw_info 'MyText'
     user nil
   end
