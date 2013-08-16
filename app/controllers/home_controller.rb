1cfd4f0b83fbf9df22489bd3628112ce1bf40ac0
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-08-16 01:53:45 -0700

    Remove unused Home controller

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-# This controller will act as the site root for unauthenticated users.
-# It does not currently do anything because the current setup will
-# automatically redirect unauthenticated users to the sign in page.
-class HomeController < ApplicationController
-  def index
-  end
-end
