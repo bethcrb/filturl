76f2cdd8124fb724a501afb0fb3f8accfca469b4
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-10-08 15:29:56 -0700

    Update response if updated before 15 minutes ago

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -7,6 +7,9 @@ class WebpagesController < ApplicationController
       @webpage_request = @webpage.webpage_requests.find_by(user_id: current_or_guest_user.id)
       if @webpage_request.present?
         @webpage_response = @webpage.webpage_responses.last
+        if (@webpage_response.updated_at < 15.minutes.ago)
+          @webpage_response.get_url
+        end
         @screenshot = @webpage.screenshot
         if user_signed_in?
           @screenshot.upload_screenshot if @screenshot.needs_update?
