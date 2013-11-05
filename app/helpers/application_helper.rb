3983cc3eb7156a363b683f67026583fe7afe7891
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2013-11-05 05:04:00 -0800

    Move Chartbeat JS to template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,34 +25,7 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
-  def chartbeat_head
-    return '' unless chartbeat_enabled?
-    javascript_tag('var _sf_startpt=(new Date()).getTime()')
-  end
-
-  def chartbeat_body
-    return '' unless chartbeat_enabled?
-    javascript_tag do
-      <<-CHARTBEAT_JS.html_safe
-      var _sf_async_config = { uid: #{ENV['CHARTBEAT_UID']}, domain: '#{ENV['CHARTBEAT_DOMAIN']}', useCanonical: true };
-      (function() {
-        function loadChartbeat() {
-          window._sf_endpt = (new Date()).getTime();
-          var e = document.createElement('script');
-          e.setAttribute('language', 'javascript');
-          e.setAttribute('type', 'text/javascript');
-          e.setAttribute('src','//static.chartbeat.com/js/chartbeat.js');
-          document.body.appendChild(e);
-        };
-        var oldonload = window.onload;
-        window.onload = (typeof window.onload != 'function') ?
-          loadChartbeat : function() { oldonload(); loadChartbeat(); };
-      })();
-      CHARTBEAT_JS
-    end
-  end
-
-  private
+  protected
 
   def chartbeat_enabled?
     ENV['CHARTBEAT_UID'].present? && ENV['CHARTBEAT_DOMAIN'].present?
