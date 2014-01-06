7f8984422b524ea86e45edc3f300b4a3637db5f4
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2014-01-06 02:03:24 -0800

    Add set_webpage_request controller method
    - Move find_or_initialize_by logic to set_webpage_request
    - Use before_action instead of before_filter
    - Update WebpageRequestsController spec examples

--- app/controllers/webpage_requests_controller.rb
+++ app/controllers/webpage_requests_controller.rb
@@ -3,18 +3,14 @@
 # successful in webpage_requests#create, it redirects to the webpages#show
 # path for the URL's effective location after accounting for redirects.
 class WebpageRequestsController < ApplicationController
-  before_filter :current_or_guest_user
+  before_action :current_or_guest_user
+  before_action :set_webpage_request, only: :create
 
   def index
     @webpage_request = WebpageRequest.new
   end
 
   def create
-    @webpage_request = WebpageRequest.find_or_initialize_by(
-      url:     PostRank::URI.clean(webpage_request_params[:url]).to_s,
-      user_id: current_or_guest_user.id
-    )
-
     respond_to do |format|
       if @webpage_request.save
         format.html { redirect_to @webpage_request.webpage }
@@ -26,6 +22,13 @@ class WebpageRequestsController < ApplicationController
   end
 
   private
+
+  def set_webpage_request
+    @webpage_request = WebpageRequest.find_or_initialize_by(
+      url:     PostRank::URI.clean(webpage_request_params[:url]).to_s,
+      user_id: current_or_guest_user.id
+    )
+  end
 
   def webpage_request_params
     params.require(:webpage_request).permit(:url)
--- spec/controllers/webpage_requests_controller_spec.rb
+++ spec/controllers/webpage_requests_controller_spec.rb
@@ -3,21 +3,47 @@ require 'spec_helper'
 describe WebpageRequestsController do
   include_context 'skip screenshot callbacks'
 
-  let(:user) { FactoryGirl.create(:user) }
+  describe "GET 'index'" do
+    before(:each) do
+      get :index
+    end
 
-  before do
-    sign_in user
+    it 'assigns @webpage_request to a new WebpageRequest' do
+      get :index
+      expect(assigns(:webpage_request)).to be_a_new(WebpageRequest)
+    end
 
-    @attr = {
-      url:     'http://www.yahoo.com',
-      user_id: user.id,
-    }
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
   end
 
   describe "POST 'create'" do
-    it 'redirects to the webpage results', :vcr do
-      post :create, { webpage_request: @attr }
-      response.should redirect_to(WebpageRequest.last.webpage)
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user }
+
+    context 'when the webpage request is successful', :vcr do
+      let(:webpage_request_params) do
+        { url: 'http://www.yahoo.com/', user_id: user.id }
+      end
+
+      it 'redirects to the webpage results' do
+        post :create, webpage_request: webpage_request_params
+        webpage_request = WebpageRequest.find_by(webpage_request_params)
+        expect(response).to redirect_to(webpage_request.webpage)
+      end
+    end
+
+    context 'when the webpage request is not successful', :vcr do
+      let(:bad_webpage_request_params) do
+        { url: 'http://not.a.valid.url/', user_id: user.id }
+      end
+
+      it 'renders the index template when there is an error' do
+        post :create, webpage_request: bad_webpage_request_params
+        expect(response).to render_template('index')
+      end
     end
   end
 end
