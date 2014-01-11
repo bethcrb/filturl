9c53a7e10f16d55fc28cfa64af38930216419fa3
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2014-01-11 09:19:15 -0800

    Add spec example for redirects

--- spec/services/webpage_service_spec.rb
+++ spec/services/webpage_service_spec.rb
@@ -1,35 +1,32 @@
 require 'spec_helper'
 
-describe 'WebpageService', vcr: { cassette_name: 'WebpageService' } do
+describe 'WebpageService' do
   include_context 'skip screenshot callbacks'
   include_context 'phantomjs'
 
-  before(:each) do
-    VCR.use_cassette('WebpageRequest/create') do
-      @webpage_request = create(:webpage_request, perform_http_request: true)
-    end
-  end
-
   describe '.new' do
+    let(:webpage_request) { build(:webpage_request) }
     it 'returns the WebpageService object' do
-      webpage_service = WebpageService.new(@webpage_request)
+      webpage_service = WebpageService.new(webpage_request)
       expect(webpage_service).to be_a(WebpageService)
     end
   end
 
-  describe '.perform_http_request' do
+  describe '.perform_http_request', :vcr do
+    let(:webpage_request) { build(:webpage_request) }
     it 'returns a Typhoeus::Response' do
-      response = WebpageService.perform_http_request(@webpage_request)
+      response = WebpageService.perform_http_request(webpage_request)
       expect(response).to be_a(Typhoeus::Response)
     end
   end
 
   describe '#perform_http_request' do
-    let(:webpage_service) { WebpageService.new(@webpage_request) }
+    let(:webpage_request) { create(:webpage_request) }
+    let(:webpage_service) { WebpageService.new(webpage_request) }
 
-    context 'when it is successful' do
+    context 'when it is successful', :vcr do
       before(:each) { webpage_service.perform_http_request }
-      
+
       it 'returns a Typhoeus::Response' do
         expect(webpage_service.response).to be_a(Typhoeus::Response)
       end
@@ -43,15 +40,30 @@ describe 'WebpageService', vcr: { cassette_name: 'WebpageService' } do
       end
 
       it 'sets the webpage_request status to "complete"' do
-        expect(@webpage_request.status).to eq('complete')
+        expect(webpage_request.status).to eq('complete')
       end
     end
 
-    context 'when save_webpage fails' do
+    context 'when save_webpage fails', :vcr do
       it 'sets the webpage_request status to "error"' do
         webpage_service.stub(:save_webpage).and_return(false)
         webpage_service.perform_http_request
-        expect(@webpage_request.status).to eq('error')
+        expect(webpage_request.status).to eq('error')
+      end
+    end
+
+    context 'when there are redirects', :vcr do
+      let(:redirect_response) do
+        Typhoeus::Response.new(response_headers: 'Location: http://localhost/')
+      end
+
+      it 'saves the redirects' do
+        webpage_service.stub(:redirections).and_return([redirect_response])
+        webpage_service.perform_http_request
+
+        webpage_redirects = webpage_request.webpage_response.webpage_redirects
+        expect(webpage_redirects.size).to eq(1)
+        expect(webpage_redirects.first.url).to eq('http://localhost/')
       end
     end
   end
