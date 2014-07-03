38b164cef066e13e537d0193a31aa91cd36e2e28
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2014-07-02 19:05:02 -0700

    Remove display_base_errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,6 @@
 # This is the base application helper module. Methods added to this helper will
 # be available to all views in the application.
 module ApplicationHelper
-  def display_base_errors(resource)
-    return '' if (resource.errors.empty?) || (resource.errors[:base].empty?)
-    messages = resource.errors[:base].map { |msg| content_tag(:p, msg) }.join
-    html = <<-HTML
-    <div class="alert alert-error alert-block">
-      <button type="button" class="close" data-dismiss="alert">&#215;</button>
-      #{messages}
-    </div>
-    HTML
-    html.html_safe
-  end
-
   def resource_name
     :user
   end
