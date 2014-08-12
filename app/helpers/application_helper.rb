3f1a013db397d320f7dbdd33c711ae7f3c32f176
Beth Coffelt-Roth-Barreiro <bethcrb@gmail.com>
2014-08-12 12:36:07 -0700

    Remove unused ApplicationHelper methods
    Remove ApplicationHelper methods that were added for guest captcha modal
    logic and now no longer necessary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,4 @@
 # This is the base application helper module. Methods added to this helper will
 # be available to all views in the application.
 module ApplicationHelper
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
 end
